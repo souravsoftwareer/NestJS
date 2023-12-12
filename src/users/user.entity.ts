35f20ee4d0691079c65e9d53d2d7d76ee00ae026
Dhawan7 <kapillikes@gmail.com>
2023-12-12 16:21:53 +0530

    auth commiting

---
+++ src/users/user.entity.ts
@@ -0,0 +1,20 @@
+import { PrimaryGeneratedColumn, BaseEntity, Column, Entity } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  eotp:String
+  
+}
